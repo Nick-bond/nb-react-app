a2014703c98233a1f515b62602ecdb904a6ed11d
nick-bond <n.jsdevelop@gmail.com>
2020-09-23 15:36:38 +0300

    update api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export default {
   API_URL: 'https://s3-eu-west-1.amazonaws.com/frontend-dev-test/recipes.json',
   API_RATING: '/api/rating',
-  API_RATING_UPDATE: '/rating/update',
+  API_RATING_UPDATE: '/api/rating/update',
   types: ['Easy', 'Normal', 'Medium', 'Hard'],
 };
